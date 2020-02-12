109a13f68423ac9208061ee0b3534113a4dceaa5
Hazor-Hellscream <support@hellscream.org>
2020-02-13 00:12:07 +1000

    [Core/script] Added npc xprate changer.

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2018 TrinityCore <https://www.trinitycore.org/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -16,10 +16,12 @@
  */
 
 // This is where scripts' loading functions should be declared:
-
+void AddSC_custom_npcs();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
+    AddSC_custom_npcs();
+
 }
