c08e0528680275ded47d66b376ab3996128c5888
Firoqt <jordan83222@gmail.com>
2020-03-06 15:42:04 +0200

    [DB/Creature] - Added one more script name for a gorgrond NPC - @Ely

--- sql/base/03_06_2020_gorgrond_npcs_part_1.sql
+++ sql/base/03_06_2020_gorgrond_npcs_part_1.sql
@@ -1,3 +1,4 @@
 -- UPDATE `creature_template` SET `ScriptName`='npc_gorgrond_podling_nibbler' WHERE `entry`=84549;
 UPDATE `creature_template` SET `ScriptName`='npc_gorgrond_podling_scavenger'  WHERE `entry`=84402;
+UPDATE `creature_template` SET `ScriptName`='npc_gorgrond_fallen_rangari'  WHERE `entry`=84663;
 -- UPDATE `creature_template` SET `ScriptName`='npc_gorgrond_gromkar_grunt_rajess'  WHERE `entry`=85266;
