2153bb470e85e5b353e5348942e5d6e334bced0b
Emirate <jordan83222@gmail.com>
2020-04-08 11:11:45 -0400

    [Core/Spell] - Fix all issues with Blade Flurry

---
+++ sql/updates/4_8_2020_spell_script_names.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=13877 AND `ScriptName`='spell_rog_blade_flurry';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (13877, 'spell_rog_blade_flurry');
