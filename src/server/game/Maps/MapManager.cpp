ab4bb7c8b2d609542ac6f78d74ad7a2982259f0e
Nyr <nyrdeveloper@gmail.com>
2020-04-18 06:13:39 +0200

    [Core/Instance] Fixed players being able to join other groups if the defeated at leas…  …t the same number of encounters on Normal raid difficulty.
    With this, Flexible Boss-based Lockouts work as intended, so the map lockouts system remake is now complete.

--- src/server/game/Maps/MapManager.cpp
+++ src/server/game/Maps/MapManager.cpp
@@ -282,17 +282,28 @@ bool MapManager::CanPlayerEnter(uint32 mapid, Player* player, bool loginCheck)
 			{
 				if (playerBoundInstance->perm && playerBoundInstance->save && leaderBoundInstance->perm && leaderBoundInstance->save)
 				{
-					// Different save instance id's.
-					if (playerBoundInstance->save->GetInstanceId() != leaderBoundInstance->save->GetInstanceId())
-					{
-						player->SendTransferAborted(entry->MapID, TRANSFER_ABORT_LOCKED_TO_DIFFERENT_INSTANCE);
-						return false;
-					}
 					// Different save defeated encounters. If the player has more, error. Else he inherits them on entrance.
 					if (playerBoundInstance->save->GetEncounterMask() > leaderBoundInstance->save->GetEncounterMask())
 					{
 						player->SendTransferAborted(entry->MapID, TRANSFER_ABORT_ALREADY_COMPLETED_ENCOUNTER);
 						return false;
+					}
+
+					// Different save instance id's.
+					if (playerBoundInstance->save->GetInstanceId() != leaderBoundInstance->save->GetInstanceId())
+					{
+						// For Normal raids, if the leader has at least the same number of defeated encounters, update the player bind to his and let him enter.
+						if (leader->GetDifficulty(entry->IsRaid()) == Difficulty10N || leader->GetDifficulty(entry->IsRaid()) == Difficulty25N)
+						{
+							player->UnbindInstance(mapid, leader->GetDifficulty(entry->IsRaid()), false);
+							player->BindToInstance(leaderBoundInstance->save, leaderBoundInstance->perm, false);
+							playerBoundInstance = player->GetBoundInstance(mapid, player->GetDifficulty(entry->IsRaid())); // Player inherits leader difficulty.
+						}
+						else // For Heroic raids, the player cannot enter.
+						{
+							player->SendTransferAborted(entry->MapID, TRANSFER_ABORT_LOCKED_TO_DIFFERENT_INSTANCE);
+							return false;
+						}
 					}
 				}
 			}
