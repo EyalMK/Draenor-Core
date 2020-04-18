f29c726036d1ebed420f8c651e8da56b5dec8e00
Nyr <nyrdeveloper@gmail.com>
2020-04-18 06:45:06 +0200

    [Core/SQL] Soothing mist statue

---
+++ sql/base/soothing_mist.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=125950 AND `ScriptName`='spell_monk_soothing_mist_statue';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (125950, 'spell_monk_soothing_mist_statue');
