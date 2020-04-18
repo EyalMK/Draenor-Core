a96630c6fad4e73deb7ddf968cacadea50e433c9
EyalMK <wezkc21@gmail.com>
2020-04-18 21:06:31 +0300

    [DB/Quest] - Edited and Final - Proving Pit Quest Fix

--- sql/base/18_04_2020_proving_pit_quest_fix.sql
+++ sql/base/18_04_2020_proving_pit_quest_fix.sql
@@ -1,1 +1,2 @@
-UPDATE `quest_template` SET `NextQuestIdChain`=26277 WHERE `ID`=26276;
+UPDATE `quest_template` SET `NextQuestIdChain`=26274 WHERE `ID`=26276;
+UPDATE `creature_queststarter` SET `Id`=42618 WHERE `Quest`=26276;
