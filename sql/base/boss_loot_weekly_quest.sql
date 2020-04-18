734191ea8065e26b2830f58d4e1f49f8e89a4a9e
Nyr <nyrdeveloper@gmail.com>
2020-04-18 08:03:37 +0200

    [SQL] Adding missing rows

--- sql/base/boss_loot_weekly_quest.sql
+++ sql/base/boss_loot_weekly_quest.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS `boss_loot_weekly_quest`;
 CREATE TABLE `boss_loot_weekly_quest` (
   `entry` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Boss Entry',
+  `difficulty` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Instance Difficulty' AFTER `entry`,
   `questId` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Quest Entry',
   `comment` text DEFAULT NULL,
-  PRIMARY KEY (`entry`,`questId`)
+  PRIMARY KEY (`entry`,`difficulty``)
 ) ENGINE=MYISAM DEFAULT CHARSET=utf8 COMMENT='Loot-based Lockout System';
--- sql/base/characters/character_weekly_boss_kills.sql
+++ sql/base/characters/character_weekly_boss_kills.sql
@@ -1,7 +1,13 @@
+/*Table structure for table `character_weekly_boss_kills` */
+
 DROP TABLE IF EXISTS `character_weekly_boss_kills`;
 CREATE TABLE `character_weekly_boss_kills` (
-  `guid` INT(10) UNSIGNED DEFAULT 0  NOT NULL,
-  `entry` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Boss Entry',
-  `looted` TINYINT(3) DEFAULT '0'   NOT NULL,
+  `guid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Player GUID',
+  `entry` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'Boss Entry',
+  `mapId` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'Map Id',
+  `difficulty` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'Difficulty',
+  `looted` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT 'Boss looted',
   PRIMARY KEY (`guid`,`entry`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='Loot-based Lockout System'; 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Loot-based Lockout System';
+
+/*Data for the table `character_weekly_boss_kills` */
