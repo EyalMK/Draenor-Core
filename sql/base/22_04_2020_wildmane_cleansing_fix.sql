1a31c76f3d02b2011ad0877aabf65baa1f9dc025
EyalMK <wezkc21@gmail.com>
2020-04-22 02:49:18 +0300

    [DB/Quest] - Implemented Wildmane Cleansing quest objective - https://github.com/Hazor-Hellscream/Project-Hellscream-Info/issues/842

---
+++ sql/base/22_04_2020_wildmane_cleansing_fix.sql
@@ -0,0 +1,3 @@
+DELETE FROM `quest_template_objective` WHERE `ID`=254222;
+INSERT INTO `quest_template_objective` (`ID`, `QuestID`, `Type`, `Index`, `ObjectID`, `Amount`, `Flags`, `UnkFloat`, `Description`, `VisualEffects`, `BuildVerified`) VALUES
+(254222, 24524, 5, 0, 4978, 1, 0, 0, 'Cleanse the Wildmane Well', '', 20779);
