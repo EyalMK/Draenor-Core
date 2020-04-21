9da94bf25434510536f472d6dfa67747d213b5b1
EyalMK <wezkc21@gmail.com>
2020-04-22 02:53:02 +0300

    [DB/Quest] - Implemented Winterhoof Cleansing quest objective - https://github.com/Hazor-Hellscream/Project-Hellscream-Info/issues/839

---
+++ sql/base/22_04_2020_winterhoof_cleansing_fix.sql
@@ -0,0 +1,3 @@
+DELETE FROM `quest_template_objective` WHERE `ID`=254215;
+INSERT INTO `quest_template_objective` (`ID`, `QuestID`, `Type`, `Index`, `ObjectID`, `Amount`, `Flags`, `UnkFloat`, `Description`, `VisualEffects`, `BuildVerified`) VALUES
+(254215, 24440, 5, 0, 4975, 1, 0, 0, 'Well Cleansed', '', 20779);
