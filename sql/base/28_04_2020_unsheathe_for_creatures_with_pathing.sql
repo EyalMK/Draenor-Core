437487874a6568eaa10ad573fbaeaf031c1d5186
EyalMK <wezkc21@gmail.com>
2020-04-28 22:31:14 +0300

    [DB/Pathing] - Implemented Unsheathe for all creatures with pathing and implemented new Ghostlands WoD creatures pathing

---
+++ sql/base/28_04_2020_unsheathe_for_creatures_with_pathing.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_addon` SET `bytes2`=2 WHERE `path_id` > 0 AND `guid` IN ( SELECT `guid` FROM `creature` WHERE `id` IN (SELECT `entry` FROM `creature_equip_template` WHERE `itemEntry1`=0 AND `itemEntry2`=0 AND `itemEntry3`>0)) AND `bytes2`=0;
+UPDATE `creature_addon` SET `bytes2`=1 WHERE `path_id` > 0 AND `guid` IN ( SELECT `guid` FROM `creature` WHERE `id` IN (SELECT `entry` FROM `creature_equip_template` WHERE `itemEntry1`>0)) AND `bytes2`=0;
