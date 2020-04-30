0ed52f383fe2e48a64c9b8eeedcee090bc4b27be
EyalMK <wezkc21@gmail.com>
2020-04-30 03:50:17 +0300

    [DB/Creature] - Added Pathing for Lathorick - Crucial for Rise, Obsidion Scripts to work - https://github.com/Hazor-Hellscream/Project-Hellscream-Core/commit/fde539ae59b1c03a95f385e896623c0db20873eb

---
+++ sql/base/30_04_2020_rise_obsidion_lathorick_pathing.sql
@@ -0,0 +1,6 @@
+DELETE FROM `waypoint_data` WHERE `id`=839100;
+INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES
+(839100, 1, -6465.226,-1268.506,180.6726, 0, 0, 0, 0, 100, 0),
+(839100, 2, -6469.583,-1266.059,180.5968, 0, 0, 0, 0, 100, 0),
+(839100, 3, -6473.7,-1260.241,180.4732, 0, 0, 0, 0, 100, 0),
+(839100, 4, -6476.368,-1252.349,180.3318, 0, 0, 0, 0, 100, 0);
