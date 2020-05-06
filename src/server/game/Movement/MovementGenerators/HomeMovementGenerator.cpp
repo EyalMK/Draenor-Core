cd59d9d6a252a505eb5e8db9886616b0e2fbf4fc
Emirate <jordan83222@gmail.com>
2020-05-06 12:26:49 -0400

    [Core/Movement] - Part 3: HomeMovementGenerator
    * Added unit_state/reset upon arrival
    * Added spline update on execution

--- src/server/game/Movement/MovementGenerators/HomeMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/HomeMovementGenerator.cpp
@@ -21,36 +21,45 @@ void HomeMovementGenerator<Creature>::DoFinalize(Creature* owner)
 {
     owner->ClearUnitState(UNIT_STATE_EVADE);
 
-    if (arrived)
-    {
-        owner->SetWalk(true);
-        owner->LoadCreaturesAddon();
-        owner->AI()->JustReachedHome();
-    }
+	if (arrived)
+	{
+		owner->ClearUnitState(UNIT_STATE_EVADE);
+		owner->SetWalk(true);
+		owner->LoadCreaturesAddon();
+		owner->AI()->JustReachedHome();
+		owner->AI()->Reset();
+	}
 }
 
 void HomeMovementGenerator<Creature>::DoReset(Creature*) { }
 
 void HomeMovementGenerator<Creature>::_setTargetLocation(Creature* owner)
 {
-    if (owner->HasUnitState(UNIT_STATE_ROOT | UNIT_STATE_STUNNED | UNIT_STATE_DISTRACTED))
-        return;
+	if (owner->HasUnitState(UNIT_STATE_ROOT | UNIT_STATE_STUNNED | UNIT_STATE_DISTRACTED))
+		return;
 
-    Movement::MoveSplineInit init(owner);
-    float x, y, z, o;
-    // at apply we can select more nice return points base at current movegen
-    if (owner->GetMotionMaster()->empty() || !owner->GetMotionMaster()->top()->GetResetPosition(owner, x, y, z))
-    {
-        owner->GetHomePosition(x, y, z, o);
-        init.SetFacing(o);
-    }
-    init.MoveTo(x, y, z);
-    init.SetWalk(false);
-    init.Launch();
+	if (!owner->movespline->Finalized())
+	{
+		owner->UpdateSplinePosition();
+		owner->movespline->_Finalize();
+	}
 
-    arrived = false;
+	Movement::MoveSplineInit init(owner);
+	float x, y, z, o;
 
-    owner->ClearUnitState(uint32(UNIT_STATE_ALL_STATE & ~(UNIT_STATE_EVADE | UNIT_STATE_IGNORE_PATHFINDING)));
+	// At apply, we can select more nice return points base at current movegen
+	if (owner->GetMotionMaster()->empty() || !owner->GetMotionMaster()->top()->GetResetPosition(owner, x, y, z))
+	{
+		owner->GetHomePosition(x, y, z, o);
+		init.SetFacing(o);
+	}
+	init.MoveTo(x, y, z);
+	init.SetWalk(false);
+	init.Launch();
+
+	arrived = false;
+
+	owner->ClearUnitState(uint32(UNIT_STATE_ALL_STATE & ~UNIT_STATE_EVADE));
 }
 
 bool HomeMovementGenerator<Creature>::DoUpdate(Creature* owner, const uint32 /*time_diff*/)
