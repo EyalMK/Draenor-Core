f0031862d412ec2129adb1f8fc734b7297f79523
EyalMK <wezkc21@gmail.com>
2020-05-08 01:03:47 +0300

    [Core/DB] - Fixed Azuregos Texts, Gossip and Teleport spell. Implemented Azuregos's pathing - Also fixed Spirit of Azuregos and gossip menu.

--- src/server/scripts/Classic/World-Bosses/boss_azuregos.cpp
+++ src/server/scripts/Classic/World-Bosses/boss_azuregos.cpp
@@ -6,7 +6,9 @@
 
 enum Say
 {
-	SAY_TELEPORT = 0
+	SAY_AGGRO	 = 0,
+	SAY_TELEPORT = 1,
+	SAY_REFLECT  = 2
 };
 
 enum Spells
@@ -39,6 +41,38 @@ class boss_azuregos : public CreatureScript
 public:
 	boss_azuregos() : CreatureScript("boss_azuregos") { }
 
+	enum eData
+	{
+		// Gossip Info
+		GOSSIP_MENU		= 15000,
+		GOSSIP_OPTION	= 0,
+		NPC_TEXT		= 61090,
+
+		// Action
+		ActionHostile	= 0,
+	};
+
+	bool OnGossipHello(Player* p_Player, Creature* p_Creature) override
+	{
+		p_Player->ADD_GOSSIP_ITEM_DB(GOSSIP_MENU, GOSSIP_OPTION, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
+		p_Player->SEND_GOSSIP_MENU(NPC_TEXT, p_Creature->GetGUID());
+
+		return true;
+	}
+
+	bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action) override
+	{
+		player->PlayerTalkClass->ClearMenus();
+
+		if (action == GOSSIP_ACTION_INFO_DEF)
+		{
+			player->CLOSE_GOSSIP_MENU();
+			creature->AI()->DoAction(ActionHostile);
+		}
+
+		return true;
+	}
+
 	struct boss_azuregosAI : public WorldBossAI
 	{
 		boss_azuregosAI(Creature* creature) : WorldBossAI(creature)
@@ -48,12 +82,16 @@ public:
 
 		void Reset() override
 		{
+			me->setFaction(7); // Neutral faction
+			me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE); // Non attackable
+			me->SetFlag(UNIT_FIELD_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP); // Gossip option
 			_Reset();
 		}
 
 		void EnterCombat(Unit* /*who*/) override
 		{
 			DoCast(me, SPELL_MARK_OF_FROST_AURA, true);
+			Talk(SAY_AGGRO); // This place is under my protection.
 			_enraged = false;
 
 			events.ScheduleEvent(EVENT_MARK_OF_FROST, 35);
@@ -69,6 +107,21 @@ public:
 		{
 			if (who->GetTypeId() == TYPEID_PLAYER)
 				who->CastSpell(who, SPELL_MARK_OF_FROST, true);
+		}
+
+		void DoAction(int32 const p_Action) override
+		{
+			switch (p_Action)
+			{
+				case ActionHostile:
+				{
+
+					me->setFaction(168); // Azuregos's Hostile Faction
+					me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+					me->RemoveFlag(UNIT_FIELD_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
+					break;
+				}
+			}
 		}
 
 		void UpdateAI(uint32 diff) override
@@ -101,16 +154,24 @@ public:
 				case EVENT_TELEPORT:
 				{
 					Talk(SAY_TELEPORT);
-				//	for (auto const& pair : me->GetPvECombatRefs())
-				//		if (Player* player = GetOther(me)->ToPlayer())
-				//			DoTeleportPlayer(player, me->GetPositionX(), me->GetPositionY(), me->GetPositionZ() + 3, player->GetOrientation());
 
-					me->DeleteThreatList();
+					std::list<HostileReference*>& m_threatlist = me->getThreatManager().getThreatList();
+					std::list<HostileReference*>::iterator i = m_threatlist.begin();
+					for (i = m_threatlist.begin(); i != m_threatlist.end(); ++i)
+					{
+						Unit* pUnit = Unit::GetUnit((*me), (*i)->getUnitGuid());
+						if (pUnit && (pUnit->GetTypeId() == TYPEID_PLAYER))
+						{
+							DoTeleportPlayer(pUnit, me->GetPositionX(), me->GetPositionY(), me->GetPositionZ() + 3, pUnit->GetOrientation());
+						}
+					}
+					DoResetThreat();
 					events.ScheduleEvent(EVENT_TELEPORT, 30);
 					break;
 				}
 				case EVENT_REFLECT:
 					DoCast(me, SPELL_REFLECT);
+					Talk(SAY_REFLECT);
 					events.ScheduleEvent(EVENT_REFLECT, 20, 35);
 					break;
 				case EVENT_CLEAVE:
@@ -166,9 +227,12 @@ public:
 	{
 		PrepareSpellScript(spell_mark_of_frost_SpellScript);
 
-		bool Validate(SpellInfo const* /*spellInfo*/) override
+		bool Validate(SpellInfo const* spellInfo) override
 		{
-		//	return validateSpellInfo({ SPELL_MARK_OF_FROST, SPELL_AURA_OF_FROST });
+			if (!sSpellMgr->GetSpellInfo(SPELL_MARK_OF_FROST))
+				return false;
+			if (!sSpellMgr->GetSpellInfo(SPELL_AURA_OF_FROST))
+				return false;
 			return true;
 		}
 
