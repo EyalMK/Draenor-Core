411e76647160407f9733298a2b8f69f95442319e
Nyr <nyrdeveloper@gmail.com>
2020-05-09 09:46:11 +0200

    [Core/Spell] Glyph of Raging Wind fix

---
+++ sql/updates/spell_war_raging_blow.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=58370 AND `ScriptName`='spell_warr_raging_wind';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (58370, 'spell_warr_raging_wind');
