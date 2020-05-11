2d67cca73c9dfdf581457d3605906fa7846669cd
Nyr <nyrdeveloper@gmail.com>
2020-05-12 00:41:12 +0200

    [Core/Spell] Priest fixes
    Item:
    
    Priest T18 Discipline 2P fixed.
    
    Priest T18 Holy 2P fixed.
    Priest T18 Holy 4P fixed.
    
    Repudiation of War (Discipline) fixed.
    Repudiation of War (Shadow) fixed.
    Repudiation of War (Holy) fixed.

---
+++ sql/updates/priest_disci_t18_2p.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=186477 AND `ScriptName`='spell_pri_t18_disci_2p';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (186477, 'spell_pri_t18_disci_2p');
---
+++ sql/updates/priest_holy_t18_2p.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=186298 AND `ScriptName`='spell_pri_t18_holy_2p';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (186298, 'spell_pri_t18_holy_2p');
