b82aad3da5c4c9839a3faa0d61e5c8584061f867
EyalMK <wezkc21@gmail.com>
2020-05-13 09:41:12 +0300

    [DB/Creatures] - Fixed Silverwind Refuge NPCs - Work In Progress

---
+++ sql/updates/13_05_2020_silverwind_refuge_couple_of_npcs.sql
@@ -0,0 +1,16 @@
+-- Ulthaan wrong dynamic flag
+UPDATE `creature_template` SET `dynamicflags`=4 WHERE `entry`=3960;
+
+-- Astranaar Sentinel
+UPDATE `creature_template` SET `dynamicflags`=4 WHERE `entry`=52000;
+
+-- Ashenvale Assassin Aura
+DELETE FROM `creature_template_addon` WHERE `entry`=34603;
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`, `animkit`) VALUES
+(34603, 0, 0, 0, 8, 0, '65726', 0);
+
+-- Silverwind Defender
+UPDATE `creature_template` SET `unit_flags`=0 WHERE `entry`=34621;
+
+-- Rampaging Furlbolg
+UPDATE `creature_template` SET `unit_flags`=0 WHERE `entry`=34620;
