9396a23433bcc1b07eee3a218f8c5bfe6a6b2608
Nyr <nyrdeveloper@gmail.com>
2020-05-14 06:19:12 +0200

    [SQL] Few orgrimmar fixes

---
+++ sql/updates/orgrimmar_grunt_actions.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_addon` WHERE `guid`=194783;
+INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`, `animkit`) VALUES (194783, 0, 0, 0, 257, 0, '42648', 0);
+DELETE FROM `creature_addon` WHERE `guid`=195474;
+INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`, `animkit`) VALUES (195474, 0, 0, 0, 257, 0, '42648', 0);
