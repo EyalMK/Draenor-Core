821894863242f220f59c1773c24526154f8e0b10
EyalMK <wezkc21@gmail.com>
2020-05-15 16:46:36 +0300

    [DB/Instance] - Fixed Access Requirements to Heroic Dungeons (SM, SH and Scholomance)

---
+++ sql/updates/15_05_2020_heroic_dungeons_requirement.sql
@@ -0,0 +1,12 @@
+DELETE FROM `access_requirement` WHERE `mapId`=1001 AND `difficulty`=2;
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`) VALUES (1001, 2, 90, 0, 0, 0, 0, 0, 0, 0, 0, '', 'Scarlet Monastery Halls');
+DELETE FROM `access_requirement` WHERE `mapId`=1001 AND `difficulty`=1;
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`) VALUES (1001, 1, 21, 0, 0, 0, 0, 0, 0, 0, 0, '', 'Scarlet Monastery Halls');
+DELETE FROM `access_requirement` WHERE `mapId`=1004 AND `difficulty`=1;
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`) VALUES (1004, 1, 23, 0, 0, 0, 0, 0, 0, 0, 0, '', 'Scarlet Monastery Cathedral');
+DELETE FROM `access_requirement` WHERE `mapId`=1004 AND `difficulty`=2;
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`) VALUES (1004, 2, 90, 0, 0, 0, 0, 0, 0, 0, 0, '', 'Scarlet Monastery Cathedral');
+DELETE FROM `access_requirement` WHERE `mapId`=1007 AND `difficulty`=1;
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`) VALUES (1007, 1, 33, 0, 0, 0, 0, 0, 0, 0, 0, '', 'Scholomance Entrance');
+DELETE FROM `access_requirement` WHERE `mapId`=1007 AND `difficulty`=2;
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `itemlevel_min`, `itemlevel_max`, `quest_failed_text`, `comment`) VALUES (1007, 2, 90, 0, 0, 0, 0, 0, 0, 0, 0, '', 'Scholomance Entrance');
