935660fa17c586f9fde5cdbce756b4afc883dfc7
Nyr <nyrdeveloper@gmail.com>
2020-05-15 23:18:38 +0200

    [SQL] Ragnaros model update REAL ONE

---
+++ sql/updates/ragnaros_model_fix_real.sql
@@ -0,0 +1,2 @@
+DELETE FROM `creature_model_info` WHERE `modelid`=11121;
+INSERT INTO `creature_model_info` (`modelid`, `bounding_radius`, `combat_reach`, `gender`, `modelid_other_gender`) VALUES (11121, 20, 20, 0, 0);
