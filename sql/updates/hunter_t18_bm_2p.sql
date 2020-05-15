a35e4275b8d22a12f9554f4b66fdb764dd762014
Nyr <nyrdeveloper@gmail.com>
2020-05-15 04:11:32 +0200

    [Core/Spell] Hunter fixes
    Item:
    
    T18 Beastmastery 2P fixed.
    T18 Beastmastery 4P fixed.
    
    T18 Marksmanship 2P fixed.
    T18 Marksmanship 4P fixed.
    
    T18 Survival 2P fixed.
    T18 Survival 4P fixed.
    
    Talisman of the Master Tracker (Beastmaster) fixed.
    Talisman of the Master Tracker (Marksmanship) fixed.
    Talisman of the Master Tracker (Survival) fixed.

---
+++ sql/updates/hunter_t18_bm_2p.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=3044 AND `ScriptName`='spell_hun_arcane_shot';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (3044, 'spell_hun_arcane_shot');
---
+++ sql/updates/hunter_t18_mm_2p.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=188190 AND `ScriptName`='spell_hun_t18_mm_2p';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (188190, 'spell_hun_t18_mm_2p');
