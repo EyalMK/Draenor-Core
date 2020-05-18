27d787881873641e08dd7d7a16d44324ab73cfa4
Nyr <nyrdeveloper@gmail.com>
2020-05-18 20:59:34 +0200

    [Core/Spell] Stat pets

---
+++ sql/updates/dogs_model.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_model_info` WHERE `modelid`=63751;
+INSERT INTO `creature_model_info` (`modelid`, `bounding_radius`, `combat_reach`, `gender`, `modelid_other_gender`) VALUES (63751, 2, 2, 2, 0);
+DELETE FROM `creature_model_info` WHERE `modelid`=63750;
+INSERT INTO `creature_model_info` (`modelid`, `bounding_radius`, `combat_reach`, `gender`, `modelid_other_gender`) VALUES (63750, 2, 2, 2, 0);
