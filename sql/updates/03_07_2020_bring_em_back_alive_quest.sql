2ddfb047000e93eba1cb4b8eaee6d5cb2c6cd982
EyalMK <wezkc21@gmail.com>
2020-07-03 16:15:44 +0300

    [SmartAI/Quest] - Bring 'Em Back Alive Quest Fixed

---
+++ sql/updates/03_07_2020_bring_em_back_alive_quest.sql
@@ -0,0 +1,6 @@
+DELETE FROM `smart_scripts` WHERE (`entryorguid`=25596 AND `source_type`=0);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(25596, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 11, 32423, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Infected Kodo Beast - On Reset - Cast 'Blue Radiation'"),
+(25596, 0, 1, 0, 27, 0, 100, 0, 0, 0, 0, 0, 91, 7, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Infected Kodo Beast - On Passenger Boarded - Remove Flag Standstate Dead"),
+(25596, 0, 2, 0, 31, 0, 100, 0, 45877, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 22, 0, 0, 0, 0, 0, 0, 0, "Infected Kodo Beast - On Target Spellhit 'Deliver Kodo' - Despawn Instant"),
+(25596,0,3,0,0,0,100,0,5000,5000,14500,16700,11,45876,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"Infected Kodo Beast - IC - Cast Stampede");
