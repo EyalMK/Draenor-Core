61dadfd534834997b430974e58d16bae7ca4f190
EyalMK <wezkc21@gmail.com>
2020-07-05 23:57:11 +0300

    [DB/Creature] - Wintergrasp vehicles missing spellclick fix

---
+++ sql/updates/03_07_2020_wintergrasp_vehicles_missing_spellclick.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `npcflag` = npcflag|16777216 WHERE `entry`= 49135; -- Skyshredder Turret
+UPDATE `creature_template` SET `npcflag` = npcflag|16777216 WHERE `entry`= 25334; -- Horde Siege Tank
+UPDATE `creature_template` SET `npcflag` = npcflag|16777216 WHERE `entry`= 29857; -- Frostbite
