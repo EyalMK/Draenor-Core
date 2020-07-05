ddc57d5f9b64822e1122136a3d31af15cf3da571
EyalMK <wezkc21@gmail.com>
2020-07-06 00:10:28 +0300

    [Core/Quest] - Silverpine Forest - Lost In Darkness Quest Fixed

---
+++ sql/updates/05_07_2020_lost_in_the_darkness_german_locale_added.sql
@@ -0,0 +1,2 @@
+-- fix quest 27093
+UPDATE locales_quest SET OfferRewardText_loc3="Lok'tar, $n. Sobald die Wirkung des Biers nachlässt, werden meine Orcs bereit sein!", QuestGiverTextWindow_loc3="Orcischer Seebär", QuestGiverTargetName_loc3="Orcischer Seebär" WHERE entry=27093;
