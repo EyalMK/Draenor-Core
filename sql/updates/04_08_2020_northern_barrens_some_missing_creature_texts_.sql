aacf7d569534f8764b3abba98f47389650fbb888
EyalMK <wezkc21@gmail.com>
2020-09-13 17:01:41 +0300

    [DB/Creature] - Northern Barrens NPCs - Missing Texts

---
+++ sql/updates/04_08_2020_northern_barrens_some_missing_creature_texts_.sql
@@ -0,0 +1,59 @@
+delete from creature_text where entry=14873; -- Okla
+insert into creature_text values
+(14873, 0, 0, "Can I go collect more rocks for my necklace, Papa?", 12, 0, 100, 0, 0, 0, ""),
+(14873, 1, 0, "Dabu! I will be careful Papa!", 12, 0, 100, 0, 0, 0, "");
+
+delete from creature_text where entry=5907; -- Kranal Fiss
+insert into creature_text values
+(5907, 0, 0, "Yes you may, Okla. But do not stray too far from home.", 12, 0, 100, 0, 0, 0, "");
+
+delete from creature_text where entry=34503;
+insert into creature_text values
+(34503, 0, 0, "%s attempts to run away in fear!", 16, 0, 100, 0, 0, 0, "combat Flee"),
+(34503, 1, 0, "%s is ensnared!", 16, 0, 100, 0, 0, 0, "");
+
+delete from creature_text where entry=34523;
+insert into creature_text values
+(34523, 0, 0, "I say nothing!", 12, 0, 100, 0, 0, 0, ""),
+(34523, 0, 1, "I not scared of you!", 12, 0, 100, 0, 0, 0, ""),
+(34523, 1, 0, "I say nothing! <snort>", 12, 0, 100, 0, 0, 0, ""),
+(34523, 2, 0, "Ow! <Snort>", 12, 0, 100, 0, 0, 0, ""),
+(34523, 2, 1, "Unf. You learn nothing from me!", 12, 0, 100, 0, 0, 0, ""),
+(34523, 2, 2, "Oof! I not talking!", 12, 0, 100, 0, 0, 0, ""),
+(34523, 2, 3, "<Snort!> That all you got?", 12, 0, 100, 0, 0, 0, ""),
+(34523, 2, 4, "Ow-oooh! I not talk for nothing!", 12, 0, 100, 0, 0, 0, ""),
+(34523, 2, 5, "<Snort!>", 12, 0, 100, 0, 0, 0, ""),
+(34523, 3, 0, "Unf - Not the face!", 12, 0, 100, 0, 0, 0, ""),
+(34523, 4, 0, "You waste your strength! Tortusk train us warriors never to talk.", 12, 0, 100, 0, 0, 0, ""),
+(34523, 4, 1, "Haha - <snort> ha - heee! <snort> Haha no stop haha <SNORT> <wheeze> Hee hee! Battlemaster Tortusk never prepared us for this!", 12, 0, 100, 0, 0, 0, ""),
+(34523, 5, 0, "Oh - I've said too much!", 12, 0, 100, 0, 0, 0, "");
+
+
+delete from creature_text where entry=34509;
+insert into creature_text values
+(34509, 0, 0, "Flat-nosed filth! Get off of my land!", 12, 0, 100, 0, 0, 0, ""),
+(34509, 1, 0, "Pigs! Scum! I will cleave your fat heads like apples!", 12, 0, 100, 0, 0, 0, ""),
+(34509, 2, 0, "I will tear your tusks out. You hear me?", 12, 0, 100, 0, 0, 0, ""),
+(34509, 3, 0, "That's right! Die, vermin! The Horde will have your heads for this.", 12, 0, 100, 0, 0, 0, ""),
+(34509, 4, 0, "I will RIP off your HEADS and EAT your filthy faces!", 12, 0, 100, 0, 0, 0, ""),
+(34509, 5, 0, "That's right, squeal you maggots. I will gut you lengthwise and roast you when we are through!", 12, 0, 100, 0, 0, 0, ""),
+(34509, 6, 0, "Nobody steals my livestock!", 12, 0, 100, 0, 0, 0, ""),
+(34509, 7, 0, "Eat axe you bacon-brained porkchops!", 12, 0, 100, 0, 0, 0, "");
+
+delete from creature_text where entry=34545;
+insert into creature_text values
+(34545, 0, 0, "%s goes into a frenzy!", 16, 0, 100, 0, 0, 0, "combat Frenzy");
+
+
+delete from creature_text where entry=16806;
+insert into creature_text values
+(16806, 0, 0, "You may be a noble but I'm the noble! The definitive article you might say!", 12, 0, 100, 0, 0, 0, "");
+
+delete from npc_text where ID=10123;
+insert into npc_text (ID, text0_0) values (10123, "This party is pointless. Those foolish nobles will never get to see Medivh.$B$BCan't they see this? I should have just stayed in Drkshire with my family instead of wasting my time here.");
+
+
+delete from creature_text where entry=14872;
+insert into creature_text values
+(14872, 0, 0, "Help! I'm being chased by a swarm of bees!", 12, 0, 100, 0, 0, 0, "");
+
