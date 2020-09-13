9a746a5eb6850a751a9be642a49014e414b1c96e
EyalMK <wezkc21@gmail.com>
2020-09-13 16:32:38 +0300

    [DB/Quest] - Haunted Quest Givers Fixed

---
+++ sql/updates/01_08_2020_haunted_quest_givers_fixed.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_questender` WHERE `id`=41058 AND `quest`=25660;
+DELETE FROM `creature_questender` WHERE `id`=41060 AND `quest`=25660;
+INSERT INTO `creature_questender` (`id`, `quest`) VALUES
+(41060, 25660);
