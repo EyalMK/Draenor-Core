c2e755fe422125b57a1f5caf4b84a3ecd5a8dfae
EyalMK <wezkc21@gmail.com>
2020-09-13 19:54:19 +0300

    [Core/DB-Quest] - Power Over the Tides Quest Fixed

---
+++ sql/updates/30_07_2020_power_over_the_tides_quest_fix.sql
@@ -0,0 +1,12 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` = '62517';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(62517, 'spell_elune_presence');
+
+-- Tranquil Tidal Spirit
+SET @ENTRY := 32937;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,54,0,100,0,0,0,0,0,33,32937,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"On Just Summoned - Give Quest Credit"); 
