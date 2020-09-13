ddce4be1c9437073469940ea9406832e6f8662ac
EyalMK <wezkc21@gmail.com>
2020-09-13 17:12:32 +0300

    [Core/DB-Quest] - The Latest Fashion - Jewelcrafting Daily Quest Fixed

---
+++ sql/updates/23_07_2020_the_latest_fashion_jc_daily_quest_fix.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (73667, 73619);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(73619, 'spell_jc_stardust'),
+(73667, 'spell_jc_stardust_no_2_triggered');
