df0482e0dd588a91ae91167bde7ab17cdef2cf10
EyalMK <wezkc21@gmail.com>
2020-09-13 16:34:20 +0300

    [DB/Quest] - Special Delivery For Brivel'Thwerp Quest Givers Fixed

---
+++ sql/updates/01_08_2020_special_delivery_for_brivelthwerp_quest_givers_fixed.sql
@@ -0,0 +1,10 @@
+DELETE FROM `creature_questender` WHERE `id`=40727 AND `quest`=28042;
+DELETE FROM `creature_questender` WHERE `id`=47383 AND `quest`=28042;
+INSERT INTO `creature_questender` (`id`, `quest`) VALUES
+(47383, 28042);
+
+
+DELETE FROM `creature_questender` WHERE `id`=40727 AND `quest`=28031;
+DELETE FROM `creature_questender` WHERE `id`=47383 AND `quest`=28031;
+INSERT INTO `creature_questender` (`id`, `quest`) VALUES
+(47383, 28031);
